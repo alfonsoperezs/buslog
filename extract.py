690e665b4649ce828ae9cc7d3c7942ec089488f4
alfonsoperezs <fonpesan@gmail.com>
2025-03-31 00:49:11 +0200

    add parameter to OCR

--- extract.py
+++ extract.py
@@ -2,11 +2,11 @@ import pytesseract
 from PIL import Image
 from exceptions import ParsingError
 
-def extract_text(image: str) -> str:
+def extract_text(image_path: str) -> str:
     """Convert a image to text
 
     Params:
-        image (str): Path to the image file.
+        image_path (str): Path to the image file.
 
     Return:
         str: Extracted text from the image.
@@ -14,7 +14,9 @@ def extract_text(image: str) -> str:
     Raises:
         FileNotFoundError: If the image file does not exist.
     """
-    return pytesseract.image_to_string(Image.open(image))
+    # TODO preprocessing the image before convert to text
+    custom_config = r'--oem 3 --psm 4'
+    return pytesseract.image_to_string(Image.open(image_path), config=custom_config)
 
 def process_text(text: str) -> list:
     """Processes a text by splitting it into lines.
@@ -43,16 +45,10 @@ def obtain_data(data: list) -> dict:
         ParsingError: If any required data is missing.
     """
     try:
-        line = next((s.split("+")[-1].strip() for s in data if s.startswith("Linea") and "+" in s))
+        line = next((s.split(": ")[-1].strip() for s in data if s.startswith("Linea") and ":" in s))
         date = next((s.split(": ")[-1] for s in data if s.startswith("Fecha") and ": " in s))
         stop = next((s.split(": ")[-1] for s in data if s.startswith("Origen") and ": " in s))
     except StopIteration:
         raise ParsingError("Can't find all necesary data")
     
     return {"Line": line, "Date": date, "Stop": stop}
-
-
-if __name__ == '__main__':
-    l = process_text(extract_text('images/test.jpg'))
-    d = obtain_data(l)
-    print(d)
